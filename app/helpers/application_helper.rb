31d50be99e674af60a9ec3039df4a820bd10ca58
Caio Ramos <caioramos97@gmail.com>
2017-09-06 10:04:26 -0300

    Formatacao dos numeros consertada

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
       when 'success' then "alert alert-success"
       when 'error' then "alert alert-danger"
       when 'alert' then "alert alert-warning"
-    end
+      end
+  end
+
+  def format_number(num)
+    number_to_currency(num, {:unit => 'R$', :separator => ',', :delimiter => '.', :precision => 2})
   end
 end
